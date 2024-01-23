715f88dc9190ea66eb183664ef1cf9fe0fd0ab20
xhyuaner <xhyuaner@gmail.com>
2024-01-23 15:18:44 +0800

    feature: add jwt middleware of restserver

---
+++ micro/server/restserver/middlewares/jwt.go
@@ -0,0 +1,126 @@
+package middlewares
+
+import (
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+type CustomClaims struct {
+	ID          uint
+	NickName    string
+	AuthorityId uint //1表示普通用户, 2表示管理员
+	jwt.StandardClaims
+}
+
+func JWTAuth(signKey string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 我们这里jwt鉴权取头部信息 x-token 登录时回返回token信息 这里前端需要把token存储到cookie或者本地localStorage中 不过需要跟后端协商过期时间 可以约定刷新令牌或者重新登录
+		token := c.Request.Header.Get("x-token")
+		if token == "" {
+			c.JSON(http.StatusUnauthorized, map[string]string{
+				"msg": "请登录",
+			})
+			c.Abort()
+			return
+		}
+		j := NewJWT(signKey)
+		// parseToken 解析token包含的信息
+		claims, err := j.ParseToken(token)
+		if err != nil {
+			if err == TokenExpired {
+				if err == TokenExpired {
+					c.JSON(http.StatusUnauthorized, map[string]string{
+						"msg": "授权已过期",
+					})
+					c.Abort()
+					return
+				}
+			}
+
+			c.JSON(http.StatusUnauthorized, "未登陆")
+			c.Abort()
+			return
+		}
+		c.Set("claims", claims)
+		c.Set("userId", claims.ID)
+		c.Next()
+	}
+}
+
+type JWT struct {
+	SigningKey []byte
+}
+
+var (
+	TokenExpired     = errors.New("token is expired")
+	TokenNotValidYet = errors.New("token not active yet")
+	TokenMalformed   = errors.New("that's not even a token")
+	TokenInvalid     = errors.New("couldn't handle this token")
+)
+
+func NewJWT(signKey string) *JWT {
+	return &JWT{
+		[]byte(signKey), //还可以设置过期时间
+	}
+}
+
+// CreateToken 创建一个token
+func (j *JWT) CreateToken(claims CustomClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(j.SigningKey)
+}
+
+// ParseToken 解析 token
+func (j *JWT) ParseToken(tokenString string) (*CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (i interface{}, e error) {
+		return j.SigningKey, nil
+	})
+	if err != nil {
+		if ve, ok := err.(*jwt.ValidationError); ok {
+			if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+				return nil, TokenMalformed
+			} else if ve.Errors&jwt.ValidationErrorExpired != 0 {
+				// Token is expired
+				return nil, TokenExpired
+			} else if ve.Errors&jwt.ValidationErrorNotValidYet != 0 {
+				return nil, TokenNotValidYet
+			} else {
+				return nil, TokenInvalid
+			}
+		}
+	}
+	if token != nil {
+		if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
+			return claims, nil
+		}
+		return nil, TokenInvalid
+
+	} else {
+		return nil, TokenInvalid
+
+	}
+
+}
+
+// RefreshToken 更新token
+func (j *JWT) RefreshToken(tokenString string) (string, error) {
+	jwt.TimeFunc = func() time.Time {
+		return time.Unix(0, 0)
+	}
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return j.SigningKey, nil
+	})
+	if err != nil {
+		return "", err
+	}
+	if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
+		jwt.TimeFunc = time.Now
+		claims.StandardClaims.ExpiresAt = time.Now().Add(1 * time.Hour).Unix()
+		return j.CreateToken(*claims)
+	}
+	return "", TokenInvalid
+}
