5941511d82cf02b7dd47a11274a59c68b1a03d5c
xhyuaner <xhyuaner@gmail.com>
2024-03-06 13:38:37 +0800

    feat: add program run entry

---
+++ cmd/my-service/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"github.com/go-kratos/kratos/v2/config"
+	"github.com/hashicorp/consul/api"
+	"mymicro/micro/app"
+	"mymicro/micro/registry"
+	"mymicro/micro/registry/consul"
+	"mymicro/pkg/log"
+)
+
+func NewApp(basename string) *app.App {
+	cfg := config.New()
+	appl := app.NewApp("my-service", basename, app.WithOptions(cfg), app.WithRunFunc(run(cfg)))
+	return appl
+}
+
+func NewRegistrar(registry *options.RegistryOptions) registry.Registrar {
+	c := api.DefaultConfig()
+	c.Address = registry.Address
+	c.Scheme = registry.Scheme
+	cli, err := cli.NewClient(c)
+	if err != nil {
+		panic(err)
+	}
+	r := consul.New(cli, consul.WithHealthCheck(true))
+	return r
+}
+
+func NewServiceApp(cfg *config.Config) (*gapp.App, error) {
+	log.Init(cfg.Log)
+	defer log.Flush()
+
+	register := NewRegistrar(cfg.Registry)
+	rpcServer, err := NewServiceHTTPServer(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return gapp.New(
+		gapp.WithName(cfg.Server.Name),
+		gapp.WithRestServer(rpcServer),
+		gapp.WithRegistrar(register),
+	), nil
+}
+
+func run(cfg *config.Config) app.RunFunc {
+	return func(baseName string) error {
+		serviceApp, err := NewServiceApp(cfg)
+		if err != nil {
+			return err
+		}
+
+		if err := serviceApp.Run(); err != nil {
+			log.Errorf("run service app error: %s", err)
+			return err
+		}
+		return nil
+	}
+}
+
+func main() {
+	NewApp("myProject")
+}
